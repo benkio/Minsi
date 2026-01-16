c08deec17eef33fd17601fae6ae2ce3d9e61e6ad
Enrico Benini <benkio89@gmail.com>
2026-01-16 13:25:54 +0000

    ➕ next steps in frontend. ➕ controls as disabled at the start

--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -23,6 +23,14 @@ program = do
     doc <- getDocument
     components <- loadComponents doc
     log "Components correctly loaded"
+    -- Initialize State
+    -- check dependencies: yt-dlp,ffmpeg...
+    -- add yt handler + FFI iframe API + enable the rest of the control + spin
+    -- add slider cut logic UI constraint: cstartmax<cendmin, cendmax < yt video length, cstart < cend values
+    -- add subtitle logic UI constraint: 0-max length of yt cut
+    -- Consider a single button (or none, with hotreload always) for elaborating async and a control to signal the video is ready
+    -- Idea: add a control for current video position to facilitate the insertion of subtitles
+    -- Idea: dropdown to switch from video to GIF to compare the 2.
 
 errorsHandler :: Error -> Effect Unit
 errorsHandler e = do
