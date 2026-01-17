1ad25e037c37b2766b896f366f68df4f04652152
Enrico Benini <benkio89@gmail.com>
2026-01-17 17:57:17 +0000

    Frontend TODOs

--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -22,13 +22,6 @@ program = do
   _ <- loadComponents doc
   log "Components correctly loaded"
 
--- Initialize State
--- add yt handler + FFI iframe API + enable the rest of the control + spin
--- add slider cut logic UI constraint: cstartmax<cendmin, cendmax < yt video length, cstart < cend values
--- add subtitle logic UI constraint: 0-max length of yt cut
--- Consider a single button (or none, with hotreload always) for elaborating async and a control to signal the video is ready
--- Idea: add a control for current video position to facilitate the insertion of subtitles
--- Idea: dropdown to switch from video to GIF to compare the 2.
 
 genericErrorsHandler :: Effect Unit -> Effect Unit
 genericErrorsHandler p = catchError p \e -> raiseErrorAlert (message e)
@@ -36,3 +29,22 @@ genericErrorsHandler p = catchError p \e -> raiseErrorAlert (message e)
 genericErrorsHandlerEither :: forall a. Either Error a -> Effect Unit
 genericErrorsHandlerEither (Right _) = pure unit
 genericErrorsHandlerEither (Left e) = raiseErrorAlert (message e)
+
+-- TODOs --------------------------------
+{-
+
+- Add the validations on the fields when the controls are converted to
+  state to not be empty. Add tests
+- Change the /cutVideo endpoint to /compute endpoint. Print the
+  incoming json to Console.
+- Add the Apply button handler that takes the htmlcontrols, convert
+  them to state nad send them to the compute endpoint if the
+  conversion is successful. disable the button until the call ends.
+- Add spin component to signal there's a computation going on.
+- add yt handler + FFI iframe API + enable the rest of the control
+- add slider cut logic UI constraint: cstartmax<cendmin, cendmax < yt video length, cstart < cend values
+-- add subtitle logic UI constraint: 0-max length of yt cut and more...
+
+-- Idea: add a control for current video position to facilitate the insertion of subtitles
+-- Idea: dropdown to switch from video to GIF to compare the 2.
+-}
