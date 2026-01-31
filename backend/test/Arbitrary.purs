4897b575f0886a656f19b3e36fa706255c344913
Enrico Benini <benkio89@gmail.com>
2026-01-31 14:00:20 +0000

    🔧 arbitrary

--- backend/test/Arbitrary.purs
+++ backend/test/Arbitrary.purs
@@ -4,14 +4,15 @@ import Data.Time.Duration (Milliseconds(..))
 import Model.State (DurationRange(..))
 import Prelude
 import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary)
-import Test.QuickCheck.Gen (suchThat)
+import Test.QuickCheck.Gen (choose, suchThat)
 
 data Range = Range Number Number
 
 instance Arbitrary Range where
   arbitrary = do
     start <- arbitrary
-    gap <- suchThat arbitrary (\g -> g > 100.0)
+    -- arbitrary for Number is uniform [0,1]; use choose so gap is always > 100
+    gap <- choose 100.1 10000.0
     pure (Range start (start + gap))
 
 -- | DurationRange that satisfies validateRange: start < end - 100 (i.e. end - start > 100 ms)
@@ -20,7 +21,7 @@ newtype ValidDurationRange = ValidDurationRange DurationRange
 instance Arbitrary ValidDurationRange where
   arbitrary = do
     start <- arbitrary
-    gap <- suchThat arbitrary (\g -> g > 100.0)
+    gap <- choose 100.1 10000.0
     pure $ ValidDurationRange $ DurationRange
       { start: Milliseconds start
       , end: Milliseconds (start + gap)
@@ -32,6 +33,7 @@ newtype InvalidDurationRange = InvalidDurationRange DurationRange
 instance Arbitrary InvalidDurationRange where
   arbitrary = do
     start <- arbitrary
+    -- arbitrary for Number is [0,1], so this predicate is always satisfied
     gap <- suchThat arbitrary (\g -> g >= 0.0 && g <= 100.0)
     pure $ InvalidDurationRange $ DurationRange
       { start: Milliseconds start
