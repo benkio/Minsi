f212bf3a917fb5fd4e9a5ef8d7d5caf8d6ea8a45
Enrico Benini <benkio89@gmail.com>
2026-02-01 19:06:23 +0000

    The modal should disappear eventually, no matter what

--- frontend/src/Components/Modal.js
+++ frontend/src/Components/Modal.js
@@ -1,15 +1,19 @@
 export function showModal(id) {
-  return function() {
+  return function () {
     const el = document.getElementById(id);
     if (el) {
       const modal = bootstrap.Modal.getOrCreateInstance(el);
       modal.show();
     }
+    // Set timeout. If after 10 sec nothing happened, something is wrong or the user saw it already.
+    setTimeout(() => {
+      hideModal(id);
+    }, 10000);
   };
 }
 
 export function hideModal(id) {
-  return function() {
+  return function () {
     const el = document.getElementById(id);
     if (el) {
       const modal = bootstrap.Modal.getOrCreateInstance(el);
