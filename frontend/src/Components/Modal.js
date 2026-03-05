87b713fa12be1404a14ea0a25362f7cc169276bf
Enrico Benini <benkio89@gmail.com>
2026-03-05 17:03:29 +0000

    Loading sound 🔉 + new version

--- frontend/src/Components/Modal.js
+++ frontend/src/Components/Modal.js
@@ -1,12 +1,16 @@
+const backgroundSound = new Audio('loadingSound.mp3')
 export function showModal(id) {
   return function (enableTimeout) {
     return function () {
       const el = document.getElementById(id);
       const modal = bootstrap.Modal.getOrCreateInstance(el);
       modal.show();
+      backgroundSound.play();
       // Set timeout. If after 10 sec nothing happened, something is wrong or the user saw it already.
       if (enableTimeout) {
         setTimeout(() => {
+          backgroundSound.pause();
+          backgroundSound.currentTime = 0;
           modal.hide(id);
         }, 10000);
       }
@@ -18,6 +22,8 @@ export function hideModal(id) {
   return function () {
     const el = document.getElementById(id);
     const modal = bootstrap.Modal.getOrCreateInstance(el);
+    backgroundSound.pause();
+    backgroundSound.currentTime = 0;
     modal.hide();
   };
 }
