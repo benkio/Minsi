681cce5d48910504a1f8a40a73bea664e0040a66
Enrico Benini <benkio89@gmail.com>
2026-05-06 13:18:48 +0100

    Implement export state button event handler

--- frontend/src/Handlers/CopyTranscriptButtonHandler.purs
+++ frontend/src/Handlers/CopyTranscriptButtonHandler.purs
@@ -10,17 +10,19 @@ import Data.Tuple (fst)
 import Effect (Effect)
 import Model.State.State (Subtitle(..))
 import Components.HtmlComponents (loadComponents)
-import Components.HtmlComponents.Lenses (_copyTranscriptButton)
+import Components.HtmlComponents.Lenses (_copyTranscriptButton, _genericModalContent)
+import Components.HtmlIdAndClasses (genericModalId)
+import Components.Modal (showModal)
 import Data.Lens (view)
 import Model.State.Lenses (_subtitles)
 import Model.State.StateFromHtml (getCurrentState)
 import Web.DOM.Element (toEventTarget)
+import Web.DOM.Node (setTextContent)
 import Web.Event.EventTarget (addEventListener, eventListener)
 import Web.Event.Internal.Types (Event)
-import Web.HTML (window)
 import Web.HTML.Event.EventTypes as E
 import Web.HTML.HTMLButtonElement as HB
-import Web.HTML.Window (promptDefault)
+import Web.HTML.HTMLPreElement as HP
 
 setCopyTranscriptButtonHandler :: Effect Unit
 setCopyTranscriptButtonHandler = do
@@ -31,9 +33,11 @@ setCopyTranscriptButtonHandler = do
 
 copyTranscriptButtonEventListener :: Event -> Effect Unit
 copyTranscriptButtonEventListener _ = do
+  components <- loadComponents
   state <- getCurrentState
   let
     subtitles = view _subtitles (fst state)
     transcript = capitalizeFirst <<< joinWith " " $ (\(Subtitle { value }) -> (toLower <<< trim) value) <$> sort subtitles
-  w <- window
-  void $ promptDefault "Copy to clipboard: Ctrl+C, Enter" transcript w
+    genericModalContentEl = view _genericModalContent components
+  setTextContent transcript (HP.toNode genericModalContentEl)
+  showModal genericModalId true
