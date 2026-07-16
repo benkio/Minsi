e761fc53ec77618df77bde0ff1c1415877045768
Enrico Benini <benkio89@gmail.com>
2026-07-16 12:23:19 +0100

    Avoid the `setInterval` to write the value on playbacks if it's the same

--- frontend/src/Handlers/InputVideo/PlaybackPositionHandler.purs
+++ frontend/src/Handlers/InputVideo/PlaybackPositionHandler.purs
@@ -4,11 +4,15 @@ import Conversion.Time (formatToMaxSixDigits)
 import Effect (Effect)
 import Handlers.InputVideo.Foreign (getPlayerCurrentTime, isPlayerReady)
 import Prelude
-import Web.DOM.Node (setTextContent)
+import Web.DOM.Node (setTextContent, textContent)
 import Web.HTML.HTMLParagraphElement as HP
 
 updatePlaybackPosition :: HP.HTMLParagraphElement -> Effect Unit
 updatePlaybackPosition playbackPosition = do
   playerReady <- isPlayerReady
   currentTime <- getPlayerCurrentTime
-  when playerReady $ setTextContent (formatToMaxSixDigits currentTime) (HP.toNode playbackPosition)
+  let
+    node = HP.toNode playbackPosition
+    newValue = formatToMaxSixDigits currentTime
+  oldValue <- textContent node
+  when (playerReady && oldValue /= newValue) $ setTextContent newValue node
--- frontend/src/Handlers/ResultMedia/Handler.purs
+++ frontend/src/Handlers/ResultMedia/Handler.purs
@@ -10,7 +10,7 @@ import Effect (Effect)
 import Effect.Timer (setInterval)
 import Handlers.ErrorHandlers (genericErrorsHandler)
 import Handlers.ResultMedia.MediaSrc (getMediaElement)
-import Web.DOM.Node (setTextContent)
+import Web.DOM.Node (setTextContent, textContent)
 import Web.HTML.HTMLMediaElement (currentTime, duration)
 import Web.HTML.HTMLParagraphElement as HP
 
@@ -24,10 +24,12 @@ updatePlaybackPosition = do
   components <- loadComponents
   let
     playbackPositionResultMedia = view _playbackPositionResultMedia components
+    node = HP.toNode playbackPositionResultMedia
   media <- getMediaElement components
   currentTime <- currentTime media
   duration <- duration media
-  let remainingTime = duration - currentTime
-  setTextContent
-    (formatToMaxSixDigits currentTime <> "/" <> formatToMaxSixDigits remainingTime)
-    (HP.toNode playbackPositionResultMedia)
+  let
+    remainingTime = duration - currentTime
+    newValue = formatToMaxSixDigits currentTime <> "/" <> formatToMaxSixDigits remainingTime
+  oldValue <- textContent node
+  when (oldValue /= newValue) $ setTextContent newValue node
